8d66f373c53b8955e6c55b00f92eaff7b9bdd831
Jared E. Knowles <jknowles@gmail.com>
2013-03-28 00:00:00 +0000

    version 0.2

--- R/statamode.R
+++ R/statamode.R
@@ -1,9 +1,50 @@
-statamode <-
-function(x) {
-  z <- table(as.vector(x))
-  m<-names(z)[z == max(z)]
-  if(length(m)==1){
-    return(m)
+statamode <- function(x, method=c("last", "stata","sample"))
+{  
+  x<-as.character(x)
+  
+  stata <- function(x){
+    z <- table(as.vector(x))
+    m<-names(z)[z == max(z)]
+    if(length(m)==1){
+      return(m)
+    }
+    return(".")
   }
-  return(".")
+  
+  sample2 <- function(x){
+    z <- table(as.vector(x))
+    m<-names(z)[z == max(z)]
+    if(length(m)==1){
+      return(m)
+    }
+    else if (length(m)>1){
+      return(sample(m,1))
+    }
+    else if (length(m)<1){
+      return(NA)
+    }
+  }
+  
+  last <- function(x){
+    z <- table(as.vector(x))
+    m<-names(z)[z == max(z)]
+    if(length(m)==1){
+      return(m)
+    }
+    else if(length(m)>1){
+      return(tail(m,1))
+    }
+    else if(length(m)<1){
+      return(NA)
+    }
+  }
+  
+  method <- match.arg(method)
+  
+  switch(method,
+         stata = temp <- stata(x),
+         sample = temp <- sample2(x),
+         last = temp <- last(x))
+  
+  return(temp)
 }
